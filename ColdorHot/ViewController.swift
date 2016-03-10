0065c4a6158e02c1f0793093ae81e8af503dd7de
chitranjan <chitranjan@chitranjans-macbook-pro.local>
2016-03-10 18:08:38 +0530

    Initial Commit

---
+++ ColdorHot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColdorHot
+//
+//  Created by chitranjan on 10/03/16.
+//  Copyright © 2016 akash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
